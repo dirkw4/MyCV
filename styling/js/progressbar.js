1046163f679e389aa17910706bf7c96567e8ec47
dirkw4 <dirk.whittington@gmail.com>
2024-03-12 13:50:17 +0200

    Add files via upload

---
+++ styling/js/progressbar.js
@@ -0,0 +1,50 @@
+ // The code below is applicable to the progress bars in the CV's Skills section.
+ // Its serves as an event listener to animate the progress bar upon opening the page so the bars can fill up.
+ 
+window.addEventListener('DOMContentLoaded', () => {
+    const progressBar1 = document.getElementById('progress-bar1');
+    progressBar1.style.width = '60%';
+
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const progressBar2 = document.getElementById('progress-bar2');
+    progressBar2.style.width = '85%';
+              
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const progressBar3 = document.getElementById('progress-bar3');
+    progressBar3.style.width = '90%';
+              
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const progressBar4 = document.getElementById('progress-bar4');
+    progressBar4.style.width = '90%';
+              
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const progressBar5 = document.getElementById('progress-bar5');
+    progressBar5.style.width = '90%';
+              
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const progressBar6 = document.getElementById('progress-bar6');
+    progressBar6.style.width = '75%';
+              
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const progressBar7 = document.getElementById('progress-bar7');
+    progressBar7.style.width = '50%';
+              
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const progressBar8 = document.getElementById('progress-bar8');
+    progressBar8.style.width = '50%';
+              
+});
